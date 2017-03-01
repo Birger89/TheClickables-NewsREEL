6b35b128c8a96c64ffb8ee5d722322ea38a5e060
Birger <birgerto@stub.ntnu.no>
2017-03-01 16:19:42 +0100

    add getRecommendations method

--- src/main/java/de/dailab/plistacontest/client/Recommender.java
+++ src/main/java/de/dailab/plistacontest/client/Recommender.java
@@ -1,5 +1,8 @@
 package de.dailab.plistacontest.client;
 
+import java.util.List;
+
+
 public class Recommender {
 
     private static RecommenderItemTable table;
@@ -13,5 +16,11 @@ public class Recommender {
     
     public void handleItemUpdate(RecommenderItem item) {
         this.table.handleItemUpdate(item);
+    }
+    
+    public List<Long> getRecommendations(RecommenderItem item) {
+        List<Long> recs = this.table.getLastItems(item);
+        
+        return recs;
     }
 }
